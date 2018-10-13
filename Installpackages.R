e65bc86ff842a5cb5def58262d9313b5b5e3631e
namkunghyunhee <shinhwahh@naver.com>
2018-10-13 17:48:25 +0900

    first commit

---
+++ Installpackages.R
@@ -0,0 +1,47 @@
+##----------------------------------------------------------------------------------
+## db
+##----------------------------------------------------------------------------------
+if("rJava" %in% installed.packages("rJava") == FALSE)install.packages("rJava")
+if("DBI" %in% installed.packages("DBI") == FALSE)install.packages("DBI")
+if("RJDBC" %in% installed.packages("RJDBC") == FALSE)install.packages("RJDBC")
+if("XML" %in% installed.packages("XML") == FALSE)install.packages("XML")
+if("memoise" %in% installed.packages("memoise") == FALSE)install.packages("memoise")
+if("readxl" %in% installed.packages("readxl") == FALSE)install.packages("readxl")
+##----------------------------------------------------------------------------------
+## string
+##----------------------------------------------------------------------------------
+if("dplyr" %in% installed.packages("dplyr") == FALSE)install.packages("dplyr")
+if("data.table" %in% installed.packages("data.table") == FALSE)install.packages("data.table")
+if("stringi" %in% installed.packages("stringi") == FALSE)install.packages("stringi")
+if("stringr" %in% installed.packages("stringr") == FALSE)install.packages("stringr")
+if("plyr" %in% installed.packages("plyr") == FALSE)install.packages("plyr")
+##----------------------------------------------------------------------------------
+## tm
+##----------------------------------------------------------------------------------
+if("KoNLP" %in% installed.packages("KoNLP") == FALSE)install.packages("KoNLP")
+if("tm" %in% installed.packages("tm") == FALSE)install.packages("tm")
+if("wordcloud" %in% installed.packages("wordcloud") == FALSE)install.packages("wordcloud")
+##----------------------------------------------------------------------------------
+## html
+##----------------------------------------------------------------------------------
+if("rvest" %in% installed.packages("rvest") == FALSE)install.packages("rvest")
+if("shiny" %in% installed.packages("shiny") == FALSE)install.packages("shiny")
+if("shinydashboard" %in% installed.packages("shinydashboard") == FALSE)install.packages("shinydashboard")
+if("shinyjs" %in% installed.packages("shinyjs") == FALSE)install.packages("shinyjs")
+##----------------------------------------------------------------------------------
+## chart
+##----------------------------------------------------------------------------------
+if("ggplot2" %in% installed.packages("ggplot2") == FALSE)install.packages("ggplot2")
+if("ggthemes" %in% installed.packages("ggthemes") == FALSE)install.packages("ggthemes")
+if("RColorBrewer" %in% installed.packages("RColorBrewer") == FALSE)install.packages("RColorBrewer")
+if("reshape" %in% installed.packages("reshape") == FALSE)install.packages("reshape")
+##----------------------------------------------------------------------------------
+## font
+##----------------------------------------------------------------------------------
+if("extrafont" %in% installed.packages("extrafont") == FALSE)install.packages("extrafont")
+##----------------------------------------------------------------------------------
+## ga
+##----------------------------------------------------------------------------------
+if("RGA" %in% installed.packages("RGA") == FALSE)install.packages("RGA")
+if("ggmap" %in% installed.packages("ggmap") == FALSE)install.packages("ggmap")
+if("googleVis" %in% installed.packages("googleVis") == FALSE)install.packages("googleVis")
